ef5cb73478c6e831bda924ad248278443a1c5271
omarJ10 <jalledomar2001@gmail.com>
2025-07-26 02:42:06 +0100

    Init

---
+++ src/test/java/org/redcrescent/bloodlink/RedcrescentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redcrescent.bloodlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcrescentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
